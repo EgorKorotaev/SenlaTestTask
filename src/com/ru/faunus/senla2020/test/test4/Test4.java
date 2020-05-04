8bcf10232ba1858f5be4bd8216f95ab65a23314a
Faunus_C <FaunusCarmel@yande.ru>
2020-05-04 14:12:30 +0300

    add 4 test

---
+++ src/com/ru/faunus/senla2020/test/test4/Test4.java
@@ -0,0 +1,33 @@
+package com.ru.faunus.senla2020.test.test4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Test4 {
+    public static void main(String[] args) {
+        CheckAndCall();
+    }
+
+    private static void CheckAndCall() {
+        System.out.print("Задание 4: введите предложение ");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String[] line = scanner.nextLine().split(" ");
+            ArrayList<String> words = new ArrayList<>(Arrays.asList(line));
+            System.out.print("Введите искомое слово: ");
+            Scanner scan = new Scanner(System.in);
+            String reference = scan.nextLine();
+            int counter = 0;
+            for (int i = 0; i < words.size(); i++) {
+                if (reference.toUpperCase().hashCode() == words.get(i).toUpperCase().hashCode()) {
+                    counter ++;
+                }
+            }
+            System.out.println("Слово уптреблялось: " + counter);
+        } catch (NumberFormatException e) {
+            System.out.println("Ошбика ввода: неверный формат чисел");
+        }
+    }
+}
--- src/com/ru/faunus/senla2020/test/test4/Test_4.java
+++
@@ -1,4 +0,0 @@
-package com.ru.faunus.senla2020.test.test4;
-
-public class Test_4 {
-}
