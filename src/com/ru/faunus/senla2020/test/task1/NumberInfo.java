ceac4edb6ec853bbd06bd54d504bc18029a378c3
Faunus_C <FaunusCarmel@yande.ru>
2020-05-04 15:53:12 +0300

    recopanathion

---
+++ src/com/ru/faunus/senla2020/test/task1/NumberInfo.java
@@ -0,0 +1,24 @@
+package com.ru.faunus.senla2020.test.task1;
+
+public class NumberInfo {
+    private final int number;
+
+    public NumberInfo(int number){
+        this.number = number;
+    }
+
+    public boolean isEven(){
+        return number % 2 == 0;
+    }
+
+    public boolean isPrime(){
+        boolean flag = false;
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+}
---
+++ src/com/ru/faunus/senla2020/test/task1/NumberInfoApplication.java
@@ -0,0 +1,28 @@
+package com.ru.faunus.senla2020.test.task1;
+
+import java.util.Scanner;
+
+public class NumberInfoApplication {
+    public static void main(String[] args) {
+        System.out.print("Задание 1: введите целое число ");
+        try (Scanner scanner = new Scanner(System.in)) {
+            if (scanner.hasNextInt()) {
+                NumberInfo number = new NumberInfo(scanner.nextInt());
+                String output = "Число " + scanner.nextInt() + " ";
+                if (number.isEven()) {
+                    output = output.concat("чётное ");
+                } else {
+                    output = output.concat("не чётное ");
+                }
+                if (number.isPrime()) {
+                    output = output.concat("и составное число");
+                } else {
+                    output = output.concat("и простое число");
+                }
+                System.out.println(output);
+            } else {
+                System.out.println("Ошбика ввода: введено не целое число");
+            }
+        }
+    }
+}
