933f4f3e69019e75cb1788a63771bb5d974e6c04
Faunus_C <FaunusCarmel@yande.ru>
2020-05-05 16:19:33 +0300

    rename class

--- src/com/ru/faunus/senla2020/test/task2/MathUtilsApplication.java
+++ src/com/ru/faunus/senla2020/test/task2/MathUtilsApplication.java
@@ -3,7 +3,7 @@ package com.ru.faunus.senla2020.test.task2;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class GslLcmApplication {
+public class MathUtilsApplication {
     public static void main(String[] args) {
         System.out.print("Задание 2: введите два целых числа ");
         try (Scanner scanner = new Scanner(System.in)) {
